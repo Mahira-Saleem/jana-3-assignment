b5b44d0abd928599e4d80dde1c5e848e4a0a9791
Mahira Saleem <mahirasaleemahmad@gmail.com>
2026-05-11 23:15:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Mahira!");
+
+
+var username = "Mahira";
+alert(username);
+
+var ans2 = "I am 20 years old";
+alert(ans2);
+
+var edu = "web and application development";
+alert(edu);
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);           
+
+document.write("This is my first JavaScript assignment.");
+
+document.write("<br> I am learning JavaScript. <br> I am enjoying it so far.");
+
+document.write("<br> Yah! I can write HTML content through JavaScript.");
+
+document.write("<br> Java is a widely used, high-level, object-oriented programming language designed by James Gosling at Sun Microsystems and released in 1995.");
+      
+document.write("<br>     ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+var name = "Mahira Khan";
+console.log(name);

8e7e87f0b905e78a3423a9ae8466e896160f3899
Mahira Saleem <mahirasaleemahmad@gmail.com>
2026-05-13 23:16:48 +0500

    Refactor script.js to remove unused variables and streamline code structure

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
 console.log("Mahira!");
 
 
-var username = "Mahira";
-alert(username);
-
-// method 01
-
-var ans2 = "I am 20 years old";
-alert(ans2);
-
-var edu = "web and application development";
-alert(edu);
-
-var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
-alert(pizza);     
-
-
-// methos 02
-
-var age = 25; 
+var age = 20; 
 alert("I am " + age + " years old");
 
-var visitCount = 5; 
-document.write("You have visited this site " + visitCount + " times.<br>");
+ var visits = 0;
+ visits = visits + 1;
+document.write("You have visited this site " + visits + " times.<br>");
 
+var birthYear = 2001;
+document.write("My birth year is " + birthYear + "<br>");
 
-
-document.write("This is my first JavaScript assignment.");
-
-document.write("<br> I am learning JavaScript. <br> I am enjoying it so far.");
-
-document.write("<br> Yah! I can write HTML content through JavaScript.");
-
-document.write("<br> Java is a widely used, high-level, object-oriented programming language designed by James Gosling at Sun Microsystems and released in 1995.");
-      
-document.write("<br>     ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+ var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
 
 var name = "Mahira Khan";
 console.log(name);
